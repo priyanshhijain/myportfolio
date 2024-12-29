403ddb54cbaa87e6e189ea1219f3b8310521a56a
priyanshhijain <prinshujain3243@gmail.com>
2024-12-29 17:58:36 +0530

    first commit

---
+++ src/components/experienceCard/ExperienceCard.js
@@ -0,0 +1,45 @@
+import React from "react";
+import { Fade } from "react-reveal";
+import "./ExperienceCard.css";
+function ExperienceCard(props) {
+  const experience = props.experience;
+  const theme = props.theme;
+  return (
+    <Fade bottom duration={2000} distance="20px">
+      <div className="experience-card-col experience-card-gap">
+        <div
+          className="experience-card experience-card-1"
+          style={{ backgroundColor: "#ffffff" }}
+        >
+  
+          <article
+            className="experience-card-body"
+            style={{
+              backgroundColor: theme.themeColor,
+              color: theme.oppositeThemeColor,
+            }}
+          >
+            <header>
+              <div className="title">
+                <h3>{experience.title}</h3>
+              </div>
+              <p className="meta">
+                <span className="pre-heading">{experience.company}</span>
+                <br />
+                <span className="author">{experience.duration}</span>
+              </p>
+              <ol
+                className="experience-card-description"
+                dangerouslySetInnerHTML={{
+                  __html: experience.description,
+                }}
+              ></ol>
+            </header>
+          </article>
+        </div>
+      </div>
+    </Fade>
+  );
+}
+
+export default ExperienceCard;
---
+++ src/portfolio.js
@@ -0,0 +1,600 @@
+/* Change this file to get your personal Porfolio */
+
+// Website related settings
+const settings = {
+  isSplash: true, // Change this to true if you want to use the splash screen.
+  useCustomCursor: false, // Change this to false if you want the good'ol cursor
+};
+
+//Home Page
+const greeting = {
+  title: "Hi",
+  title2: " I'm Priyanshi jain",
+  logo_name: "Priyanshi jain",
+  // nickname: "harry / picleric",
+  full_name: "Priyanshi jain",
+  subTitle1: "Frontend Developer.",
+  subTitle2: "Life Long Learner.",
+  subTitle: "Frontend Developer, Life Long Learner.",
+  resumeLink:
+    "https://drive.google.com/file/d/12t3ReH8WLxsQX2OGw2pS7blIhPtLrykP/view?usp=sharing",
+  mail: "Priyanshij251@gmail.com",
+};
+
+const socialMediaLinks = {
+  /* Your Social Media Link */
+  github: "https://github.com/sourabh-gedar",
+  linkedin: "https://www.linkedin.com/in/sourabh-gedar-a75991188/",
+  gmail: "sourabhgedar30@gmail.com",
+};
+
+//icons website
+// https://icon-sets.iconify.design/simple-icons/nodedotjs/
+
+const skills = {
+  data: [
+    {
+      _id: "354c54b7-ffd8-40f1-972f-1d006733df25",
+      title: "Development skils",
+      fileName: "FullStackImg",
+      skills: [
+        "⚡ Develop highly interactive user interfaces using backend API web applications",
+        "⚡ Building responsive website front end using React",
+      ],
+      softwareSkills: [
+        {
+          skillName: "HTML5",
+          fontAwesomeClassname: "simple-icons:html5",
+          style: {
+            color: "#E34F26",
+          },
+        },
+        {
+          skillName: "CSS3",
+          fontAwesomeClassname: "fa-css3",
+          style: {
+            color: "#1572B6",
+          },
+        },
+        {
+          skillName: "JavaScript",
+          fontAwesomeClassname: "logos:javascript",
+          style: {
+            backgroundColor: "#FFFFFF",
+            color: "#F7DF1E",
+          },
+        },
+    
+        {
+          skillName: "ReactJS",
+          fontAwesomeClassname: "simple-icons:react",
+          style: {
+            color: "#61DAFB",
+          },
+        },
+        {
+          skillName: "Next Js",
+          fontAwesomeClassname: "simple-icons:nextdotjs",
+          style: {
+            color: "#00000",
+          },
+        },
+       
+        {
+          skillName: "Redux",
+          fontAwesomeClassname: "simple-icons:redux",
+          style: {
+            color: "#593D88",
+          },
+        },
+        // {
+        //   skillName: "Angular",
+        //   fontAwesomeClassname: "simple-icons:angular",
+        //   style: {
+        //     color: "#dd0031",
+        //   },
+        // },
+        {
+          skillName: "NodeJS",
+          fontAwesomeClassname: "simple-icons:nodedotjs",
+          style: {
+            color: "#339933",
+          },
+        },
+        {
+          skillName: "Express",
+          fontAwesomeClassname: "simple-icons:express",
+          style: {
+            color: "gray",
+          },
+        },
+        {
+          skillName: "Material UI",
+          fontAwesomeClassname: "simple-icons:uikit",
+          style: {
+            color: "#007FFF",
+          },
+        },
+        {
+          skillName: "Tailwind CSS",
+          fontAwesomeClassname: "logos:tailwindcss-icon",
+          style: {
+            color: "#635BFF",
+            width: "1em",
+            height: "1em",
+          },
+        },
+      
+      
+        {
+          skillName: "Bootstrap",
+          fontAwesomeClassname: "simple-icons:bootstrap",
+          style: {
+            color: "#563d7c",
+          },
+        },
+        {
+          skillName: "Postman",
+          fontAwesomeClassname: "simple-icons:postman",
+          style: {
+            color: "#FF6C37",
+          },
+        },
+  
+        {
+          skillName: "MongoDB",
+          fontAwesomeClassname: "simple-icons:mongodb",
+          style: {
+            color: "#439743",
+          },
+        },
+      
+    
+        {
+          skillName: "Git",
+          fontAwesomeClassname: "simple-icons:git",
+          style: {
+            color: "#E94E32",
+          },
+        },
+        {
+          skillName: "Python",
+          fontAwesomeClassname: "simple-icons:python",
+          style: {
+            color: "#3776AB",
+          },
+        },
+      ],
+    },
+    {
+      _id: "c026fe64-1542-42b0-84ab-9311840c169f",
+      
+      skills: [
+  
+        "⚡ Experience hosting and managing websites",
+        "⚡ Experience with Continuous Integration",
+      ],
+      softwareSkills: [
+        {
+          skillName: "Google Cloud Platform",
+          fontAwesomeClassname: "skill-icons:gcp-dark",
+          style: {
+            backgroundColor: "transparent",
+            width: "1em",
+            height: "1em",
+          },
+        },
+   
+        {
+          skillName: "Netlify",
+          fontAwesomeClassname: "simple-icons:netlify",
+          style: {
+            color: "#38AFBB",
+            width: "1em",
+            height: "1em",
+          },
+        },
+   
+        {
+          skillName: "PostgreSQL",
+          fontAwesomeClassname: "simple-icons:postgresql",
+          style: {
+            color: "#336791",
+            width: "1em",
+            height: "1em",
+          },
+        },
+        {
+          skillName: "MongoDB",
+          fontAwesomeClassname: "simple-icons:mongodb",
+          style: {
+            color: "#47A248",
+            width: "1em",
+            height: "1em",
+          },
+        },
+        // {
+        //   skillName: "Docker",
+        //   fontAwesomeClassname: "simple-icons:docker",
+        //   style: {
+        //     color: "#1488C6",
+        //   },
+        // },
+        {
+          skillName: "GitHub Actions",
+          fontAwesomeClassname: "simple-icons:githubactions",
+          style: {
+            color: "#5b77ef",
+            width: "1em",
+            height: "1em",
+          },
+        },
+      ],
+    },
+  ],
+};
+
+const degrees = {
+  degrees: [
+    {
+      title: "Arts and Science College, Ratlam",
+      subtitle: "Master's in Computer Science",
+      logo_path: "",
+      alt_name: "Arts and Science College",
+      duration: "2021 - 2023",
+      descriptions: [
+        "⚡ I completed my Master's in Computer Science, with a focus on advanced topics in software engineering, database management, and artificial intelligence.",
+        "⚡ Additionally, I enhanced my skills by earning a Full Stack Python Developer certification from Pune in 2023."
+      ],
+      website_link: "http://www.example-asc-ratlam.com/",
+    },
+    {
+      title: "Arts and Science College, Ratlam",
+      subtitle: "Bachelor's in Computer Science",
+      logo_path: "",
+      alt_name: "Arts and Science College",
+      duration: "2018 - 2021",
+      descriptions: [
+        "⚡ I gained a strong foundation in core computer science subjects such as Data Structures, Algorithms, Operating Systems, and Artificial Intelligence.",
+        "⚡ This degree prepared me for advanced studies and professional certifications in computer science."
+      ],
+      website_link: "http://www.example-asc-ratlam.com/",
+    }
+  ]
+  
+};
+
+const certifications = {
+  certifications: [
+    // {
+    //   title: "Data Science Orientation",
+    //   subtitle: "",
+    //   logo_path: "new-ibm.png",
+    //   certificate_link:
+    //     "https://www.credly.com/badges/4e6f10ae-9510-41cc-a41f-2a280169dfd7?source=linked_in_profile",
+    //   alt_name: "Meta",
+    //   color_code: "#ffff",
+    // },
+    // {
+    //   title: "Data Science Foundations - Level 1",
+    //   subtitle: "",
+    //   logo_path: "new-ibm.png",
+    //   certificate_link:
+    //     "https://www.credly.com/badges/8a40f5db-8f34-4335-a9f1-24f9c16ea692?source=linked_in_profile",
+    //   alt_name: "Meta",
+    //   color_code: "#43EDD",
+    // },
+    // {
+    //   title: "Python for Data Science",
+    //   subtitle: "",
+    //   logo_path: "new-ibm.png",
+    //   certificate_link:
+    //     "https://www.credly.com/badges/72a40ce1-c5d0-4570-9a3a-840751733f02?source=linked_in_profile",
+    //   alt_name: "Duke University",
+    //   color_code: "#ffff",
+    // },
+    // {
+    //   title: "React Basics",
+    //   subtitle: "",
+    //   logo_path: "meta_logo.png",
+    //   certificate_link:
+    //     "https://coursera.org/share/29c75a387fcaf2f1e2403f73473092b2",
+    //   alt_name: "Meta",
+    //   color_code: "#096ce1",
+    // },
+    // {
+    //   title: "Programming with JavaScript",
+    //   subtitle: "",
+    //   logo_path: "meta_logo.png",
+    //   certificate_link:
+    //     "https://coursera.org/share/cb44f1ee2db3f50b83a56cbae15fcc74",
+    //   alt_name: "Meta",
+    //   color_code: "#096ce1",
+    // },
+    // {
+    //   title: "HTML and CSS in depth",
+    //   subtitle: "",
+    //   logo_path: "meta_logo.png",
+    //   certificate_link:
+    //     "https://coursera.org/share/667de02731a480b76b5300c6612d174d",
+    //   alt_name: "Meta",
+    //   color_code: "#096ce1",
+    // },
+    // {
+    //   title: "Java Programming: Principles of Software Design",
+    //   subtitle: "",
+    //   logo_path: "duke-logo.jpg",
+    //   certificate_link:
+    //     "https://coursera.org/share/f4e9af47fbb24381e04031f3cc8cfd9f",
+    //   alt_name: "Duke University",
+    //   color_code: "#001a57",
+    // },
+    // {
+    //   title: "Java Programming: Solving Problems with Software",
+    //   subtitle: "",
+    //   logo_path: "duke-logo.jpg",
+    //   certificate_link:
+    //     "https://coursera.org/share/36411fcea7ea4fbb5c613ef4911f4668",
+    //   alt_name: "Duke University",
+    //   color_code: "#001a57",
+    // },
+    // {
+    //   title: "Programming Foundations with JavaScript, HTML and CSS",
+    //   subtitle: "",
+    //   logo_path: "duke-logo.jpg",
+    //   certificate_link:
+    //     "https://coursera.org/share/0269c76c33997304d1842d4eef16815b",
+    //   alt_name: "Duke University",
+    //   color_code: "#001a57",
+    // },
+    // {
+    //   title: "Android Application Development - II",
+    //   subtitle: "- Dr. Adam Porter",
+    //   logo_path: "maryland_logo.svg",
+    //   certificate_link:
+    //     "https://www.coursera.org/account/accomplishments/verify/GN48B7W9TU59",
+    //   alt_name: "Stanford University",
+    //   color_code: "#e11a2b",
+    // },
+    // {
+    //   title: "Android Application Development - I",
+    //   subtitle: "- Dr. Adam Porter",
+    //   logo_path: "maryland_logo.svg",
+    //   certificate_link:
+    //     "https://www.coursera.org/account/accomplishments/verify/2H59Z22DDSU8",
+    //   alt_name: "Stanford University",
+    //   color_code: "#e11a2b",
+    // },
+    // {
+    //   title: "Fullstack Development",
+    //   subtitle: "- Christian Hur",
+    //   logo_path: "coursera_logo.png",
+    //   certificate_link:
+    //     "https://www.coursera.org/account/accomplishments/verify/97WF8EZ4Y2YD",
+    //   alt_name: "Coursera",
+    //   color_code: "#0056d2",
+    // },
+    // {
+    //   title: "ML and AI in GCP",
+    //   subtitle: "- GCP Training",
+    //   logo_path: "gcp_logo.png",
+    //   certificate_link:
+    //     "https://www.cloudskillsboost.google/public_profiles/345a17d9-689f-4154-a3d3-c0396c98b3bc/badges/2862832",
+    //   alt_name: "GCP",
+    //   color_code: "#2fa9508a",
+    // },
+    // {
+    //   title: "Foundational Infrastructure in GCP",
+    //   subtitle: "- GCP Training",
+    //   logo_path: "gcp_logo.png",
+    //   certificate_link:
+    //     "https://www.cloudskillsboost.google/public_profiles/345a17d9-689f-4154-a3d3-c0396c98b3bc/badges/2653409",
+    //   alt_name: "GCP",
+    //   color_code: "#4087f08a",
+    // },
+    // {
+    //   title: "Networking & Security in GCP",
+    //   subtitle: "- GCP Training",
+    //   logo_path: "gcp_logo.png",
+    //   certificate_link:
+    //     "https://www.cloudskillsboost.google/public_profiles/345a17d9-689f-4154-a3d3-c0396c98b3bc/badges/2510430",
+    //   alt_name: "GCP",
+    //   color_code: "#eb40308a",
+    // },
+    // {
+    //   title: "Cloud Computing Fundamentals in GCP",
+    //   subtitle: "- GCP Training",
+    //   logo_path: "gcp_logo.png",
+    //   certificate_link:
+    //     "https://www.cloudskillsboost.google/public_profiles/345a17d9-689f-4154-a3d3-c0396c98b3bc/badges/2469792",
+    //   alt_name: "GCP",
+    //   color_code: "#fabd008a",
+    // },
+  ],
+};
+
+// Experience Page
+const experience = {
+  title: "Experience",
+  subtitle: "Work and Apprenticeship",
+  description:
+    " I have 1 year of experience as a Software Developer, having contributed to projects for Calibre Technologies and working on a range of personal and collaborative projects.",
+  header_image_path: "experience.svg",
+  sections: {
+    title: "Work Experience",
+    experiences: [
+      {
+        title: "Software Engineer",
+        company: "Calibre Technologies",  
+        company_url: "https://codezilla.io/",
+        logo_path: "codezilla-logo.svg",
+        alt_name: "",
+        duration: "(Dec, 2023 - Dec, 2024)",
+        location: "Remote, Navi Mumbai",
+        description:
+        "<li>Contributed to the Radio Panel project, collaborating on feature development and bug fixes.</li><li>Participated in developing an e-commerce platform for an NGO, enhancing the user interface and backend functionality.</li><li>Worked on various smaller tasks, demonstrating versatility and attention to detail.</li>",
+      color: "#0071C5",
+      }
+    
+  
+     
+    ],
+  },
+};
+
+// Projects Page
+const projectsHeader = {
+  title: "Projects",
+  description:
+    "I have experience working as a front-end developer on various projects, including personal endeavors listed below",
+  avatar_image_path: "projects_image.svg",
+};
+
+const projects = {
+  data: [
+   
+    {
+      id: "environment-life-foundation",
+      name: "Environment Life Foundation",
+      url: "https://environmentlifefoundation.com/",
+      description:
+        "I have worked on the Environment Life Foundation, an NGO project based in Navi Mumbai focused on tree plantation and environmental sustainability. My role involved building and maintaining the website to showcase their initiatives, manage volunteer information, and facilitate donor contributions.",
+      languages: [
+        {
+          name: "Next Js",
+          iconifyClass: "logos:nextjs-icon",
+        },
+      
+       
+        {
+          name: "Bootstrap",
+          iconifyClass: "logos:bootstrap",
+        },
+        {
+          name: "SCSS",
+          iconifyClass: "vscode-icons:file-type-scss2",
+        },
+        {
+          name: "NPM",
+          iconifyClass: "devicon:npm-wordmark",
+        },
+        {
+          name: "Javascript",
+          iconifyClass: "logos:javascript",
+        },
+       
+      ],
+    }
+,    
+   
+    {
+      id: "project-2",
+      name: "Foodyhub",
+      url: "https://foodyhub.vercel.app/",
+      repoUrl: "https://github.com/sourabh-gedar/foodyhub",
+      description:
+        "Foodyhub is a full stack restaurant web application that allows you to order food online. It has wide variety of categories on the menu like Chicken, Curry, Rice, Fish, Fruits, Icecreams, Soft Drinks etc.",
+      languages: [
+        {
+          name: "React",
+          iconifyClass: "logos:react",
+        },
+        {
+          name: "Redux",
+          iconifyClass: "logos:redux",
+        },
+        {
+          name: "Firebase",
+          iconifyClass: "logos:firebase",
+        },
+        {
+          name: "Tailwind",
+          iconifyClass: "vscode-icons:file-type-tailwind",
+        },
+        {
+          name: "Framer Motion",
+          iconifyClass: "logos:framer",
+        },
+      ],
+    },
+    {
+      id: "project-3",
+      name: "E-commerce",
+      url: "https://shoplane-ecommerce.netlify.app",
+      repoUrl: "",
+      description:
+        "I have developed an e-commerce project using Fetch API and Redux Toolkit, enabling users to browse products, add them to the cart, and manage their orders efficiently.",
+      languages: [
+        {
+          name: "React",
+          iconifyClass: "logos:react",
+        },
+        {
+          name: "Talwind",
+          // iconifyClass: "vscode-icons:file-type-css",
+        },
+        {
+          name: "Bootstrap",
+          iconifyClass: "logos:bootstrap",
+        },
+      ],
+    },
+    {
+      id: "project-4",
+      name: "Portfolio",
+      url: "",
+      repoUrl: "https://github.com/sourabh-gedar/my-portfolio",
+      description: "You are watching it right now 😉.",
+      languages: [
+        {
+          name: "React",
+          iconifyClass: "logos-react",
+        },
+        {
+          name: "HTML5",
+          iconifyClass: "vscode-icons:file-type-html",
+        },
+        {
+          name: "CSS3",
+          iconifyClass: "vscode-icons:file-type-css",
+        },
+        {
+          name: "JavaScript",
+          iconifyClass: "logos-javascript",
+        },
+        {
+          name: "React Reveal",
+          iconifyClass: "simple-icons:scrollreveal",
+        },
+        {
+          name: "Styled Components",
+          iconifyClass: "vscode-icons:file-type-styled",
+        },
+      ],
+    },
+
+  ],
+};
+
+// Contact Page
+const contactPageData = {
+  contactSection: {
+    title: "Contact Me",
+    profile_image_path: "portfolioProfileImage.png",
+    description:
+      "You can contact me at the places mentioned below. I will try to get back to you as fast as I can. ",
+  },
+};
+
+export {
+  settings,
+  greeting,
+  socialMediaLinks,
+  skills,
+  degrees,
+  certifications,
+  experience,
+  projectsHeader,
+  contactPageData,
+  projects,
+};
